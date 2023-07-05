0ec91fff774cd41f3c8b2a7f76bf84ceab079153
Shashindu <shashinduchamika99@gmail.com>
2023-07-06 00:49:25 +0530

    sn7

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ const db = mysql.createPool({
 // }))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://stupendous-donut-035fa3.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://cheerful-dieffenbachia-1c946f.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
