1c3ba17a046ac164b17de793d3e30dd0698bd1c8
Shashindu <shashinduchamika99@gmail.com>
2023-07-06 22:45:49 +0530

    sn7

--- backend/index.js
+++ backend/index.js
@@ -37,17 +37,17 @@ const db   = mysql.createPool({
 //         console.log('Query results:', results);
 //       });
 // })
-// app.use(cors({
-//     origin:['https://cheerful-dieffenbachia-1c946f.netlify.app'],
-//     methods:["GET","POST"],
-//     credentials:true
-// }))
-
 app.use(cors({
-    origin:['http://localhost:3000'],
+    origin:['https://cheerful-dieffenbachia-1c946f.netlify.app'],
     methods:["GET","POST"],
     credentials:true
 }))
+
+// app.use(cors({
+//     origin:['http://localhost:3000'],
+//     methods:["GET","POST"],
+//     credentials:true
+// }))
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
