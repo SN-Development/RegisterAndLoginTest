54b6dda710c5bba8af03a3a7c101c72eebb80b31
Shashindu <shashinduchamika99@gmail.com>
2023-07-07 12:17:28 +0530

    sn7

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ const db = mysql.createPool({
 //     credentials:true
 // }))
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://reg-log.onrender.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://reg-log.onrender.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
